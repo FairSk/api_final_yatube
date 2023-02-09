5530906dbea884cfd3ef835e3b92ee87624dde6e
Никита Бачу <fair.nik@yandex.ru>
2023-02-09 14:08:06 +0400

    pre-follow stage

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,42 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Post, Group
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (CommentSerializer,
+                          GroupSerializer, PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+    pagination_class = None
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
+
+    def get_queryset(self):
+        return self.get_post().comments
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+    pagination_class = None
